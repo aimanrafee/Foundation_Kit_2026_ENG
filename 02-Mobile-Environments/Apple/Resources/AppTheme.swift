b565494c3f426f115f77ae8d0919e0ccabcc3bf3
aimanrafee <39325438+aimanrafee@users.noreply.github.com>
2026-01-30 01:05:22 +0800

    Create AppTheme.swift
    feat: implement modular Apple directory structure (MVVM)

---
+++ 02-Mobile-Environments/Apple/Resources/AppTheme.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct AppTheme {
+    static let primaryBlue = Color(red: 0.0, green: 0.4, blue: 0.8)
+    static let backgroundGray = Color(white: 0.95)
+    static let successGreen = Color.green
+    static let errorRed = Color.red
+}
